588b401da7448e866a0d518ff1803cc8adc15c85
AJ Manning <ajmanning@tampabay.rr.com>
2022-04-18 20:41:16 -0400

    Finish submarine cable map generation

--- CableMatching/data/parse_scn_map.py
+++ CableMatching/data/parse_scn_map.py
@@ -11,18 +11,38 @@ with open("../../config.json", 'r') as f:
     config = json.load(f)
     bing_api_key = config["bing_api"]
 
-with open("../../Geolocation/serv_data/country_name_code.json", 'r') as f:
+with open("../../Geolocation/serv_data/country_name_code.json", 'r', encoding='utf-8') as f:
     country_code_map = json.load(f)
 
+# I was having Unicode encoding problems; this is my 'manual' fix.
+country_code_accent_map = {
+    b'R\xc3\x83\xc2\xa9union': 'RE',
+    b"C\xc3\x83\xc2\xb4te d'Ivoire": 'CI',
+    b'Cura\xc3\x83\xc2\xa7ao': 'CW',
+    b'Saint Barth\xc3\x83\xc2\xa9lemy': "BL"
+}
+
 def find_country_code(country: str):
+    if country.encode('utf-8') in country_code_accent_map:
+        return country_code_accent_map[country.encode('utf-8')]
+
     if country in country_code_map:
         return country_code_map[country]
     else:
         for key in country_code_map:
             if country in key:
                 return country_code_map[key]
+    print(f"No country code found for {country} with length {len(country)}", flush=True)
+    return None
 
 def create_scn_map_json():
+    ### As of April 18th, 2022, this does not work for the following cables:
+    # bt-mt-1.json: Groudle Bay, Isle of Man not on Bing
+    # eastern-caribbean-fiber-system-ecfs.json: Saint Maarten, Sint Maarten not on Bing
+    # havhingstenceltixconnect-2-cc-2.json: Port Grenaugh, Isle of Man not on Bing
+    # lanis-1.json: Port Grenaugh, Isle of Man not on Bing
+    # saba-statia-cable-system-sscs.json: Great Bay Beach, Sint Maarten not on Bing
+    
     scn_map_json = []
 
     for filename in tqdm(os.listdir(path_to_scn), desc="Cable JSON files"):
@@ -50,12 +70,11 @@ def create_scn_map_json():
                         num_commas = landing['name'].count(',')
                         bing_url = None
 
-                        # City, Country
-                        if num_commas == 1:
-                            city, country = landing['name'].split(',')
+                        # City, Country; or City, Congo, [Dem. Rep./Rep.]
+                        if num_commas == 1 or "Congo" in landing['name']:
+                            city, country = landing['name'].split(',', 1)
                             country = find_country_code(country.strip())
                             bing_url = f"http://dev.virtualearth.net/REST/v1/Locations?countryRegion={country}&locality={city}&key={bing_api_key}"
-                            # print(bing_url)
 
                         # City, Region, Country
                         elif num_commas == 2:
@@ -63,7 +82,6 @@ def create_scn_map_json():
                             region = region.strip()
                             country = find_country_code(country.strip())
                             bing_url = f"http://dev.virtualearth.net/REST/v1/Locations?countryRegion={country}&adminDistrict={region}&locality={city}&key={bing_api_key}"
-                            # print(bing_url)
 
                         # one case: Kihei, Maui, HI, United States (City, Sub-region, Region, Country)
                         elif num_commas == 3:
@@ -72,7 +90,7 @@ def create_scn_map_json():
                             country = find_country_code(country.strip())
                             bing_url = f"http://dev.virtualearth.net/REST/v1/Locations?countryRegion={country}&adminDistrict={region}&locality={city}&key={bing_api_key}"
                         else:
-                            print(f"Unknown landing location format: {landing['name']} for cable {current_cable['name']}")
+                            print(f"Unknown landing location format: {landing['name']} for cable {current_cable['name']}", flush=True)
                             continue
                         
                         r = requests.get(bing_url, timeout=5)
@@ -85,10 +103,11 @@ def create_scn_map_json():
                             
                 scn_map_json.append(current_cable)
         except Exception as e:
-            print(f"Error for {filename} with exception {e}")
+            print(f"Error for {filename} with exception {e}", flush=True)
     
     with open('submarine_cable_map_NEW.json', 'w') as f:
         json.dump(scn_map_json, f, indent=4)
 
 create_scn_map_json()
 
+# TODO: create country_cable and country_hop_cable functions
